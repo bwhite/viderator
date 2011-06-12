86aef05934e9c317b0b870e30b119579183a185f
Brandyn A. White <bwhite@dappervision.com>
2011-06-12 16:28:46 -0400

    Added more sophisticated frame skip

--- vidfeat/main.py
+++ vidfeat/main.py
@@ -22,6 +22,28 @@ def frame_iter(stream, image_modes):
             break
 
 
+def frame_iter_skip(stream, image_modes, skip):
+    SEEK_START_ATTEMPTS = 3
+    # Use seek to find the first good frame
+    for i in range(SEEK_START_ATTEMPTS):
+        try:
+            stream.tv.seek_to_frame(i)
+        except IOError:
+            continue
+        else:
+            break
+    fps = stream.tv.get_fps()
+    num = None
+    while 1:
+        _, num, frame = stream.tv.get_current_frame()[:3]
+        yield num, num / fps, imfeat.convert_image(frame, image_modes)
+        try:
+            stream.tv.seek_to_frame(num + skip)
+        except IOError:
+            break
+
+
+
 def convert_video(video, modes):
     """
     Args:
@@ -39,5 +61,10 @@ def convert_video(video, modes):
             return video
         elif modes[0] == 'frameiter':
             return frame_iter(video, modes[1])
+        elif modes[0] == 'frameiterskip':
+            if modes[2] == 1:
+                return frame_iter(video, modes[1])
+            else:
+                return frame_iter_skip(video, modes[1], modes[2])
     else:
         raise ValueError('Unknown image type')
